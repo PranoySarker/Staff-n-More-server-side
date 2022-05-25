70942fef60e6249cb34edb294c798624d46473e6
PranoySarker <pranoysarker01@gmail.com>
2022-05-26 03:17:54 +0600

    getter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -21,6 +21,13 @@ async function run() {
             const query = {};
             const cursor = toolCollection.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: (ObjectId(id)) };
+            const result = await toolCollection.findOne(query);
             res.send(result);
         })
 
